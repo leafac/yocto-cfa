908373d749cb508a37755535e2fb66a0fea43a26
Leandro Facchinetti <me@leafac.com>
2019-07-19 23:47:16 -0400

    Add ;

--- implementing-a-program-analyzer-from-scratch/step-0--base-interpreter.test.ts
+++ implementing-a-program-analyzer-from-scratch/step-0--base-interpreter.test.ts
@@ -3,7 +3,7 @@ import * as _ from "lodash";
 type Expression = Function_ | Call | Variable;
 type Function_ = { kind: "Function"; variable: Variable; body: Expression; }
 type Call = { kind: "Call"; function: Expression; argument: Expression; }
-type Variable = { kind: "Variable"; name: string };
+type Variable = { kind: "Variable"; name: string; };
 
 type Value = Function_;
 
