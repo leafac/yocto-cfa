a0c5f0dfe8bb7f90798e80490329156c49683e9d
Leandro Facchinetti <me@leafac.com>
2019-07-21 05:27:43 -0400

    ' → "

--- implementing-a-program-analyzer-from-scratch/0-base-interpreter.test.ts
+++ implementing-a-program-analyzer-from-scratch/0-base-interpreter.test.ts
@@ -1,22 +1,22 @@
-import * as _ from 'lodash'
+import * as _ from "lodash"
 
 namespace YJS {
   export type Expression = Function | Call | Variable
 
   export type Function = {
-    kind: 'Function'
+    kind: "Function"
     variable: Variable
     body: Expression
   }
 
   export type Call = {
-    kind: 'Call'
+    kind: "Call"
     function: Expression
     argument: Expression
   }
 
   export type Variable = {
-    kind: 'Variable'
+    kind: "Variable"
     name: string
   }
 }
@@ -25,12 +25,12 @@ type Value = YJS.Function
 
 function evaluate(expression: YJS.Expression): Value {
   switch (expression.kind) {
-    case 'Function': return expression
-    case 'Call':
+    case "Function": return expression
+    case "Call":
       const { variable, body } = evaluate(expression.function)
       const argument = evaluate(expression.argument)
       return evaluate(substitute(variable, body, argument))
-    case 'Variable': throw new Error(`Undefined variable ‘${expression}’.`)
+    case "Variable": throw new Error(`Undefined variable ‘${expression}’.`)
   }
 }
 
@@ -39,11 +39,11 @@ function substitute(
 ): YJS.Expression {
   function traverse(in_: YJS.Expression): YJS.Expression {
     switch (in_.kind) {
-      case 'Function':
+      case "Function":
         return _.isEqual(in_.variable, variable) ? in_ : { ...in_, body: traverse(in_.body) }
-      case 'Call':
+      case "Call":
         return { ...in_, function: traverse(in_.function), argument: traverse(in_.argument) }
-      case 'Variable':
+      case "Variable":
         return _.isEqual(in_, variable) ? for_ : in_
     }
   }
@@ -51,40 +51,40 @@ function substitute(
   return traverse(in_)
 }
 
-describe('evaluate()', () => {
-  test('a function is already a value', () => {
+describe("evaluate()", () => {
+  test("a function is already a value", () => {
     expect(evaluate({
-      kind: 'Function',
-      variable: { kind: 'Variable', name: 'x' },
-      body: { kind: 'Variable', name: 'x' }
+      kind: "Function",
+      variable: { kind: "Variable", name: "x" },
+      body: { kind: "Variable", name: "x" }
     })).toMatchObject({
-      kind: 'Function',
-      variable: { kind: 'Variable', name: 'x' },
-      body: { kind: 'Variable', name: 'x' }
+      kind: "Function",
+      variable: { kind: "Variable", name: "x" },
+      body: { kind: "Variable", name: "x" }
     })
   })
 
-  test('arguments are substituted in function bodies', () => {
+  test("arguments are substituted in function bodies", () => {
     expect(evaluate({
-      kind: 'Call',
+      kind: "Call",
       function: {
-        kind: 'Function',
-        variable: { kind: 'Variable', name: 'x' },
-        body: { kind: 'Variable', name: 'x' }
+        kind: "Function",
+        variable: { kind: "Variable", name: "x" },
+        body: { kind: "Variable", name: "x" }
       },
       argument: {
-        kind: 'Function',
-        variable: { kind: 'Variable', name: 'y' },
-        body: { kind: 'Variable', name: 'y' }
+        kind: "Function",
+        variable: { kind: "Variable", name: "y" },
+        body: { kind: "Variable", name: "y" }
       }
     })).toMatchObject({
-      kind: 'Function',
-      variable: { kind: 'Variable', name: 'y' },
-      body: { kind: 'Variable', name: 'y' }
+      kind: "Function",
+      variable: { kind: "Variable", name: "y" },
+      body: { kind: "Variable", name: "y" }
     })
   })
 
-  test('programs with undefined variables throw an error', () => {
-    expect(() => evaluate({ kind: 'Variable', name: 'x' })).toThrow()
+  test("programs with undefined variables throw an error", () => {
+    expect(() => evaluate({ kind: "Variable", name: "x" })).toThrow()
   })
 })
