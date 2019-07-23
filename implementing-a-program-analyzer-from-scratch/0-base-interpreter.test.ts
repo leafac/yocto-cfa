e36b4fd59d0e5c1c14eea9ae4c747fb37549e5f2
Leandro Facchinetti <git@leafac.com>
2019-07-23 10:45:58 -0400

    Convert to esprima

--- implementing-a-program-analyzer-from-scratch/0-base-interpreter.test.ts
+++ implementing-a-program-analyzer-from-scratch/0-base-interpreter.test.ts
@@ -1,150 +1,90 @@
+import * as esprima from "esprima"
+import * as estree from "estree"
 import * as _ from "lodash"
 
-namespace YoctoJavaScript {
-  export type Expression = Function | Call | Variable
+///////////////////////////////////////////////////////////////////////////////
+// YOCTO-JAVASCRIPT
 
-  export type Function = {
-    readonly kind: "Function"
-    readonly variable: Variable
-    readonly body: Expression
-  }
+export type Expression = ArrowFunctionExpression | CallExpression | Identifier
 
-  export type Call = {
-    readonly kind: "Call"
-    readonly function: Expression
-    readonly argument: Expression
-  }
-
-  export type Variable = {
-    readonly kind: "Variable"
-    readonly name: string
-  }
+export interface ArrowFunctionExpression {
+  type: "ArrowFunctionExpression"
+  params: [Identifier]
+  body: Expression
 }
 
-type Value = YoctoJavaScript.Function
-
-function evaluate(expression: YoctoJavaScript.Expression): Value {
-  switch (expression.kind) {
-    case "Function": return expression
-    case "Call":
-      const { variable, body } = evaluate(expression.function)
-      const argument = evaluate(expression.argument)
-      return evaluate(substitute(variable, body, argument))
-    case "Variable": throw new Error(`Undefined variable ‘${expression}’.`)
-  }
+export interface CallExpression {
+  type: "CallExpression"
+  callee: Expression
+  arguments: [Expression]
 }
 
-function substitute(
-  variable: YoctoJavaScript.Variable,
-  in_: YoctoJavaScript.Expression,
-  for_: YoctoJavaScript.Expression
-): YoctoJavaScript.Expression {
-  return traverse(in_)
+export interface Identifier {
+  type: "Identifier"
+  name: string
+}
 
-  function traverse(in_: YoctoJavaScript.Expression): YoctoJavaScript.Expression {
-    switch (in_.kind) {
-      case "Function":
-        return _.isEqual(in_.variable, variable) ? in_ : { ...in_, body: traverse(in_.body) }
-      case "Call":
-        return { ...in_, function: traverse(in_.function), argument: traverse(in_.argument) }
-      case "Variable":
-        return _.isEqual(in_, variable) ? for_ : in_
+export function YoctoJavaScript(strings: TemplateStringsArray): Expression {
+  // TODO: Check
+  return (esprima.parseScript(strings[0]).body[0] as estree.ExpressionStatement).expression as Expression
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// EVALUATOR
+
+export type Value = ArrowFunctionExpression
+
+export function evaluate(expression: Expression): Value {
+  switch (expression.type) {
+    case "ArrowFunctionExpression": return expression
+    case "CallExpression":
+      const { params: [param], body } = evaluate(expression.callee)
+      const argument = evaluate(expression.arguments[0])
+      return evaluate(substitute(param, body, argument))
+    case "Identifier": throw new Error(`Unbound ‘${expression.name}’.`)
+  }
+
+  function substitute(
+    param: Identifier,
+    body: Expression,
+    argument: Value
+  ): Expression {
+    return traverse(body)
+
+    function traverse(expression: Expression): Expression {
+      switch (expression.type) {
+        case "ArrowFunctionExpression":
+          return _.isEqual(expression.params[0], param) ? expression : { ...expression, body: traverse(expression.body) }
+        case "CallExpression":
+          return { ...expression, callee: traverse(expression.callee), arguments: [traverse(expression.arguments[0])] }
+        case "Identifier":
+          return _.isEqual(expression, param) ? argument : expression
+      }
     }
   }
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// TESTS
+
 describe("evaluate()", () => {
   test("a function is already a value", () => {
-    expect(evaluate({
-      kind: "Function",
-      variable: { kind: "Variable", name: "x" },
-      body: { kind: "Variable", name: "x" }
-    })).toEqual({
-      kind: "Function",
-      variable: { kind: "Variable", name: "x" },
-      body: { kind: "Variable", name: "x" }
-    })
+    expect(evaluate(YoctoJavaScript`x => x`)).toEqual(YoctoJavaScript`x => x`)
   })
 
   test.each([
-    [
-      {
-        kind: "Call",
-        function: {
-          kind: "Function",
-          variable: { kind: "Variable", name: "x" },
-          body: { kind: "Variable", name: "x" }
-        },
-        argument: {
-          kind: "Function",
-          variable: { kind: "Variable", name: "y" },
-          body: { kind: "Variable", name: "y" }
-        }
-      },
-      {
-        kind: "Function",
-        variable: { kind: "Variable", name: "y" },
-        body: { kind: "Variable", name: "y" }
-      }
-    ],
-    [
-      {
-        kind: "Call",
-        function: {
-          kind: "Function",
-          variable: { kind: "Variable", name: "x" },
-          body: {
-            kind: "Function",
-            variable: { kind: "Variable", name: "z" },
-            body: { kind: "Variable", name: "x" }
-          }
-        },
-        argument: {
-          kind: "Function",
-          variable: { kind: "Variable", name: "y" },
-          body: { kind: "Variable", name: "y" }
-        }
-      },
-      {
-        kind: "Function",
-        variable: { kind: "Variable", name: "z" },
-        body: {
-          kind: "Function",
-          variable: { kind: "Variable", name: "y" },
-          body: { kind: "Variable", name: "y" }
-        }
-      }
-    ]
-  ] as [YoctoJavaScript.Expression, Value][])(
+    [YoctoJavaScript`(x => x) (y => y)`, YoctoJavaScript`y => y`],
+    [YoctoJavaScript`(x => z => x) (y => y)`, YoctoJavaScript`z => y => y`]
+  ] as [Expression, Value][])(
     "a call substitutes the argument in the function body",
     (program, expectedValue) => { expect(evaluate(program)).toEqual(expectedValue) }
   )
 
-  test("substitution doesn’t affect shadowed variables", () => {
-    expect(evaluate({
-      kind: "Call",
-      function: {
-        kind: "Function",
-        variable: { kind: "Variable", name: "x" },
-        body: {
-          kind: "Function",
-          variable: { kind: "Variable", name: "x" },
-          body: { kind: "Variable", name: "x" }
-        }
-      },
-      argument: {
-        kind: "Function",
-        variable: { kind: "Variable", name: "y" },
-        body: { kind: "Variable", name: "y" }
-      }
-    })).toEqual({
-      kind: "Function",
-      variable: { kind: "Variable", name: "x" },
-      body: { kind: "Variable", name: "x" }
-    })
+  test("substitution doesn’t affect shadowed Identifiers", () => {
+    expect(evaluate(YoctoJavaScript`(x => x => x) (y => y)`)).toEqual(YoctoJavaScript`x => x`)
   })
 
-  test("programs with undefined variables throw an error", () => {
-    expect(() => evaluate({ kind: "Variable", name: "x" })).toThrow()
+  test("programs with undefined Identifiers throw an error", () => {
+    expect(() => evaluate(YoctoJavaScript`x`)).toThrow()
   })
 })
