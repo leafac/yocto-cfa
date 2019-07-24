ad56f49c37ad4e268f58f70a1b31b931673faeca
Leandro Facchinetti <git@leafac.com>
2019-07-24 09:23:01 -0400

    Remove spaces in function calls

--- implementing-a-program-analyzer-from-scratch/step-0--base-interpreter.test.ts
+++ implementing-a-program-analyzer-from-scratch/step-0--base-interpreter.test.ts
@@ -7,8 +7,8 @@ describe("evaluate()", () => {
   })
 
   test.each([
-    [YJS`(x => x) (y => y)`, YJS`y => y`],
-    [YJS`(x => z => x) (y => y)`, YJS`z => y => y`]
+    [YJS`(x => x)(y => y)`, YJS`y => y`],
+    [YJS`(x => z => x)(y => y)`, YJS`z => y => y`]
   ])(
     "a call substitutes the argument in the function body",
     (program, expectedValue) => {
@@ -17,11 +17,11 @@ describe("evaluate()", () => {
   )
 
   test("substitution doesn’t affect shadowed Identifiers", () => {
-    expect(evaluate(YJS`(x => x => x) (y => y)`)).toEqual(YJS`x => x`)
+    expect(evaluate(YJS`(x => x => x)(y => y)`)).toEqual(YJS`x => x`)
   })
 
   test("substitution doesn’t affect other Identifiers", () => {
-    expect(evaluate(YJS`(x => z => z) (y => y)`)).toEqual(YJS`z => z`)
+    expect(evaluate(YJS`(x => z => z)(y => y)`)).toEqual(YJS`z => z`)
   })
 
   test("programs with unbound Identifiers throw an error", () => {
