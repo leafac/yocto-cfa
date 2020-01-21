96f4ea02fc65e2aa578ae1107a946b078c3aeba1
Leandro Facchinetti <git@leafac.com>
2020-01-21 14:42:57 -0500

    .

--- code/developing-an-analyzer/step-0--substitution-based-interpreter.test.ts
+++ code/developing-an-analyzer/step-0--substitution-based-interpreter.test.ts
@@ -1,5 +1,69 @@
-import { sum } from "./step-0--substitution-based-interpreter";
+import { parse } from "./step-0--substitution-based-interpreter";
 
-test("a", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("parser", () => {
+  test("a valid program", () => {
+    expect(parse("(x => x)(y => y)")).toMatchInlineSnapshot(`
+      Object {
+        "arguments": Array [
+          Object {
+            "body": Object {
+              "name": "y",
+              "type": "Identifier",
+            },
+            "params": Array [
+              Object {
+                "name": "y",
+                "type": "Identifier",
+              },
+            ],
+            "type": "ArrowFunctionExpression",
+          },
+        ],
+        "callee": Object {
+          "body": Object {
+            "name": "x",
+            "type": "Identifier",
+          },
+          "params": Array [
+            Object {
+              "name": "x",
+              "type": "Identifier",
+            },
+          ],
+          "type": "ArrowFunctionExpression",
+        },
+        "type": "CallExpression",
+      }
+    `);
+  });
+
+  test.each([
+    ["Line 1: Unexpected end of input", "x =>"],
+    [
+      "‘Program’ has a ‘body’ whose length isn’t exactly one.",
+      "x => x; y => y"
+    ],
+    [
+      "‘Program’ has a ‘body’ that isn’t an ‘ExpressionStatement’.",
+      "const f = x => x;"
+    ],
+    [
+      "‘ArrowFunctionExpression’ doesn’t have exactly one ‘param’.",
+      "(x, y) => x"
+    ],
+    [
+      "‘ArrowFunctionExpression’ has a ‘param’ that isn’t an ‘Identifier’.",
+      "([x, y]) => x"
+    ],
+    [
+      "‘CallExpression’ doesn’t have exactly one ‘argument’.",
+      "f => a => b => f(a, b)"
+    ],
+    ["Variable reference to ‘y’ not in scope.", "x => y"],
+    [`Invalid node type: ‘Literal’.`, "29"]
+  ])("error case: %s", (error, input) => {
+    expect(() => {
+      parse(input);
+    }).toThrow(error);
+  });
 });
--- code/developing-an-analyzer/step-0--substitution-based-interpreter.ts
+++ code/developing-an-analyzer/step-0--substitution-based-interpreter.ts
@@ -1,3 +1,81 @@
-export function sum(a: number, b: number): number {
-  return a + b;
+import { parseScript } from "esprima";
+import * as ESTree from "estree";
+
+type Expression = ArrowFunctionExpression | CallExpression | Identifier;
+
+interface ArrowFunctionExpression {
+  type: "ArrowFunctionExpression";
+  params: [Identifier];
+  body: Expression;
+}
+
+interface CallExpression {
+  type: "CallExpression";
+  callee: Expression;
+  arguments: [Expression];
+}
+
+interface Identifier {
+  type: "Identifier";
+  name: string;
+}
+
+export function parse(input: string): Expression {
+  const program = parseScript(input);
+  if (program.body.length !== 1)
+    throw new Error("‘Program’ has a ‘body’ whose length isn’t exactly one.");
+  if (program.body[0].type !== "ExpressionStatement")
+    throw new Error(
+      "‘Program’ has a ‘body’ that isn’t an ‘ExpressionStatement’."
+    );
+  return checkAndCleanup(program.body[0].expression, []);
+
+  function checkAndCleanup(
+    node: ESTree.Node,
+    variablesInScope: string[]
+  ): Expression {
+    switch (node.type) {
+      case "ArrowFunctionExpression":
+        if (node.params.length !== 1)
+          throw new Error(
+            "‘ArrowFunctionExpression’ doesn’t have exactly one ‘param’."
+          );
+        if (node.params[0].type !== "Identifier")
+          throw new Error(
+            "‘ArrowFunctionExpression’ has a ‘param’ that isn’t an ‘Identifier’."
+          );
+        return {
+          type: node.type,
+          params: [
+            {
+              type: node.params[0].type,
+              name: node.params[0].name
+            }
+          ],
+          body: checkAndCleanup(node.body, [
+            ...variablesInScope,
+            node.params[0].name
+          ])
+        };
+      case "CallExpression":
+        if (node.arguments.length !== 1)
+          throw new Error(
+            "‘CallExpression’ doesn’t have exactly one ‘argument’."
+          );
+        return {
+          type: node.type,
+          callee: checkAndCleanup(node.callee, variablesInScope),
+          arguments: [checkAndCleanup(node.arguments[0], variablesInScope)]
+        };
+      case "Identifier":
+        if (!variablesInScope.includes(node.name))
+          throw new Error(`Variable reference to ‘${node.name}’ not in scope.`);
+        return {
+          type: node.type,
+          name: node.name
+        };
+      default:
+        throw new Error(`Invalid node type: ‘${node.type}’.`);
+    }
+  }
 }
