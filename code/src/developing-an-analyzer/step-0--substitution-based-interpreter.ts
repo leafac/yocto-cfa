7209df189583392fa3482f639ff880437ba53409
Leandro Facchinetti <git@leafac.com>
2020-02-03 13:30:38 -0500

    .

--- code/src/developing-an-analyzer/step-0--substitution-based-interpreter.ts
+++ code/src/developing-an-analyzer/step-0--substitution-based-interpreter.ts
@@ -62,41 +62,6 @@ function run(expression: Expression): Value {
   }
 }
 
-// function run(expression: Expression): Value {
-//   switch (expression.type) {
-//     case "ArrowFunctionExpression":
-//       return expression;
-//     case "CallExpression":
-//       const {
-//         params: [{ name }],
-//         body
-//       } = run(expression.callee);
-//       const argument = run(expression.arguments[0]);
-//       return run(substitute(body));
-//       function substitute(expression: Expression): Expression {
-//         switch (expression.type) {
-//           case "ArrowFunctionExpression":
-//             if (expression.params[0].name === name) return expression;
-//             return {
-//               ...expression,
-//               body: substitute(expression.body)
-//             };
-//           case "CallExpression":
-//             return {
-//               ...expression,
-//               callee: substitute(expression.callee),
-//               arguments: [substitute(expression.arguments[0])]
-//             };
-//           case "Identifier":
-//             if (expression.name === name) return argument;
-//             return expression;
-//         }
-//       }
-//     case "Identifier":
-//       throw new Error(`Reference to undefined variable: ${expression.name}`);
-//   }
-// }
-
 function load(input: string): Expression {
   const program = parseScript(input, {}, node => {
     switch (node.type) {
