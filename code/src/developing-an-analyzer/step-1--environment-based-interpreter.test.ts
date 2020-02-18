8c7a6765f894d4fc0078f891a694046f0f3bba41
Leandro Facchinetti <git@leafac.com>
2020-02-18 11:23:24 -0500

    .

---
+++ code/src/developing-an-analyzer/step-1--environment-based-interpreter.test.ts
@@ -0,0 +1,97 @@
+import { evaluate } from "./step-1--environment-based-interpreter";
+
+describe("run()", () => {
+  testEvaluate("An Expression That Already Is a Value", "x => x", "x => x");
+
+  testEvaluate(
+    "A Call Involving Immediate Functions",
+    "(x => x)(y => y)",
+    "y => y"
+  );
+
+  testEvaluate(
+    "Substitution in Function Definitions",
+    "(x => z => x)(y => y)",
+    "z => y => y"
+  );
+
+  testEvaluate("Name Mismatch", "(x => z => z)(y => y)", "z => z");
+
+  testEvaluate("Name Reuse", "(x => x => x)(y => y)", "x => x");
+
+  testEvaluate(
+    "Substitution in Function Calls",
+    "(x => z => x(x))(y => y)",
+    "z => (y => y)(y => y)"
+  );
+
+  testEvaluate(
+    "An Argument That Is Not Immediate",
+    "(x => z => x)((a => a)(y => y))",
+    "z => y => y"
+  );
+
+  testEvaluate(
+    "A Function That Is Not Immediate",
+    "((z => z)(x => x))(y => y)",
+    "y => y"
+  );
+
+  testEvaluate(
+    "Continuing to Run After a Function Call",
+    "(x => (z => z)(x))(y => y)",
+    "y => y"
+  );
+
+  testEvaluateError("Reference to undefined variable: y", "(x => y)(y => y)");
+
+  testEvaluateError(
+    "Maximum call stack size exceeded",
+    "(f => f(f))(f => f(f))"
+  );
+});
+
+describe("parse()", () => {
+  testEvaluateError("Line 1: Unexpected end of input", "x =>");
+
+  testEvaluateError(
+    "Unsupported Yocto-JavaScript feature: Program with multiple statements",
+    "x => x; y => y"
+  );
+
+  testEvaluateError(
+    "Unsupported Yocto-JavaScript feature: SequenceExpression",
+    "(x, y) => x"
+  );
+
+  testEvaluateError(
+    "Unsupported Yocto-JavaScript feature: ArrayExpression",
+    "([x, y]) => x"
+  );
+
+  testEvaluateError(
+    "Unsupported Yocto-JavaScript feature: CallExpression with multiple arguments",
+    "f(a, b)"
+  );
+
+  testEvaluateError(`Unsupported Yocto-JavaScript feature: Literal`, "29");
+
+  testEvaluateError(
+    `Unsupported Yocto-JavaScript feature: VariableDeclarator`,
+    "const f = x => x"
+  );
+});
+
+function testEvaluate(name: string, input: string, expectedOutput: string) {
+  test(name, () => {
+    expect(evaluate(input)).toEqual(expectedOutput);
+  });
+}
+
+function testEvaluateError(name: string, input: string) {
+  test(name, () => {
+    expect(() => {
+      evaluate(input);
+    }).toThrow(name);
+  });
+}
---
+++ code/src/developing-an-analyzer/step-1--environment-based-interpreter.ts
@@ -0,0 +1,109 @@
+import { parseScript } from "esprima";
+import { Node } from "estree";
+import { generate } from "escodegen";
+import { format } from "prettier";
+
+export function evaluate(input: string): PrettyValue {
+  return prettify(run(parse(input), new Map()));
+}
+
+type Expression = ArrowFunctionExpression | CallExpression | Identifier;
+
+interface ArrowFunctionExpression {
+  type: "ArrowFunctionExpression";
+  params: [Identifier];
+  body: Expression;
+}
+
+interface CallExpression {
+  type: "CallExpression";
+  callee: Expression;
+  arguments: [Expression];
+}
+
+interface Identifier {
+  type: "Identifier";
+  name: string;
+}
+
+type Value = Closure;
+
+interface Closure {
+  function: ArrowFunctionExpression;
+  environment: Environment;
+}
+
+type Environment = Map<string, Value>;
+
+function run(expression: Expression, environment: Environment): Value {
+  switch (expression.type) {
+    case "ArrowFunctionExpression":
+      return { function: expression, environment };
+    case "CallExpression":
+      const {
+        function: {
+          params: [{ name: parameter }],
+          body
+        },
+        environment: functionEnvironment
+      } = run(expression.callee, environment);
+      const argument = run(expression.arguments[0], environment);
+      return run(
+        body,
+        // TODO: Double-check that tests cover the common mistake of saying ‘environment’ on the line below.
+        new Map([...functionEnvironment, [parameter, argument]])
+      );
+    case "Identifier":
+      const { name } = expression;
+      if (!environment.has(name))
+        throw new Error(`Reference to undefined variable: ${name}`);
+      return environment.get(name)!;
+  }
+}
+
+function parse(input: string): Expression {
+  const program = parseScript(input, {}, checkFeatures);
+  const expression = (program as any).body[0].expression as Expression;
+  return expression;
+  function checkFeatures(node: Node): void {
+    switch (node.type) {
+      case "Program":
+        if (node.body.length !== 1)
+          throw new Error(
+            "Unsupported Yocto-JavaScript feature: Program with multiple statements"
+          );
+        break;
+      case "ExpressionStatement":
+        break;
+      case "ArrowFunctionExpression":
+        break;
+      case "CallExpression":
+        if (node.arguments.length !== 1)
+          throw new Error(
+            "Unsupported Yocto-JavaScript feature: CallExpression with multiple arguments"
+          );
+        break;
+      case "Identifier":
+        break;
+      default:
+        throw new Error(`Unsupported Yocto-JavaScript feature: ${node.type}`);
+    }
+  }
+}
+
+interface PrettyValue {
+  function: string;
+  environment: Map<string, PrettyValue>;
+}
+
+function prettify(value: Value): PrettyValue {
+  return {
+    function: format(generate(value.function), {
+      parser: "babel",
+      semi: false
+    }).trim(),
+    environment: new Map(
+      [...value.environment].map(([name, value]) => [name, prettify(value)])
+    )
+  };
+}
