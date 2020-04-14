1fc794c43cef3081629a8aeb4763e58f470238e9
Leandro Facchinetti <git@leafac.com>
2020-04-14 14:51:17 -0400

    .

---
+++ code/src/developing-an-analyzer/step-2--store-based-interpreter.test.ts
@@ -0,0 +1,393 @@
+import { evaluate } from "./step-2--store-based-interpreter";
+
+describe("run()", () => {
+  test("§ An Expression That Already Is a Value", () => {
+    expect(evaluate("x => x")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"x => x\\",
+          \\"environment\\": []
+        },
+        \\"store\\": []
+      }"
+    `);
+  });
+
+  test("§ A Call Involving Immediate Functions", () => {
+    expect(evaluate("(x => x)(y => y)")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"y => y\\",
+          \\"environment\\": []
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+  });
+
+  test("§ Substitution in Function Definitions", () => {
+    expect(evaluate("(x => z => x)(y => y)")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"z => x\\",
+          \\"environment\\": [
+            [
+              \\"x\\",
+              0
+            ]
+          ]
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+  });
+
+  test("§ Name Mismatch", () => {
+    expect(evaluate("(x => z => z)(y => y)")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"z => z\\",
+          \\"environment\\": [
+            [
+              \\"x\\",
+              0
+            ]
+          ]
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+  });
+
+  test("§ Name Reuse", () => {
+    expect(evaluate("(x => x => x)(y => y)")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"x => x\\",
+          \\"environment\\": [
+            [
+              \\"x\\",
+              0
+            ]
+          ]
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+    expect(evaluate("(x => x => z => x)(a => a)(y => y)"))
+      .toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"z => x\\",
+          \\"environment\\": [
+            [
+              \\"x\\",
+              1
+            ]
+          ]
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"a => a\\",
+              \\"environment\\": []
+            }
+          ],
+          [
+            1,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+  });
+
+  test("§ Substitution in Function Calls", () => {
+    expect(evaluate("(x => z => x(x))(y => y)")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"z => x(x)\\",
+          \\"environment\\": [
+            [
+              \\"x\\",
+              0
+            ]
+          ]
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+  });
+
+  test("§ An Argument That Is Not Immediate", () => {
+    expect(evaluate("(x => z => x)((a => a)(y => y))")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"z => x\\",
+          \\"environment\\": [
+            [
+              \\"x\\",
+              1
+            ]
+          ]
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ],
+          [
+            1,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+  });
+
+  test("§ A Function That Is Not Immediate", () => {
+    expect(evaluate("((z => z)(x => x))(y => y)")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"y => y\\",
+          \\"environment\\": []
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"x => x\\",
+              \\"environment\\": []
+            }
+          ],
+          [
+            1,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+  });
+
+  test("§ Continuing to Run After a Function Call", () => {
+    expect(evaluate("(x => (z => z)(x))(y => y)")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"y => y\\",
+          \\"environment\\": []
+        },
+        \\"store\\": [
+          [
+            0,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ],
+          [
+            1,
+            {
+              \\"function\\": \\"y => y\\",
+              \\"environment\\": []
+            }
+          ]
+        ]
+      }"
+    `);
+  });
+
+  test("§ A Reference to an Undefined Variable", () => {
+    expect(() => {
+      evaluate("(x => y)(y => y)");
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Reference to undefined variable: y"`
+    );
+    expect(evaluate("x => y")).toMatchInlineSnapshot(`
+      "{
+        \\"value\\": {
+          \\"function\\": \\"x => y\\",
+          \\"environment\\": []
+        },
+        \\"store\\": []
+      }"
+    `);
+  });
+});
+
+describe("parse()", () => {
+  test("Syntax error", () => {
+    expect(() => {
+      evaluate("x =>");
+    }).toThrowErrorMatchingInlineSnapshot(`"Line 1: Unexpected end of input"`);
+  });
+
+  test("Program with multiple statements", () => {
+    expect(() => {
+      evaluate("x => x; y => y");
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported Yocto-JavaScript feature: Program with multiple statements"`
+    );
+  });
+
+  test("Function of multiple parameters", () => {
+    expect(() => {
+      evaluate("(x, y) => x");
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported Yocto-JavaScript feature: SequenceExpression"`
+    );
+  });
+
+  test("Function with parameter that is a pattern", () => {
+    expect(() => {
+      evaluate("([x, y]) => x");
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported Yocto-JavaScript feature: ArrayExpression"`
+    );
+  });
+
+  test("Call with multiple arguments", () => {
+    expect(() => {
+      evaluate("f(a, b)");
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported Yocto-JavaScript feature: CallExpression with multiple arguments"`
+    );
+  });
+
+  test("Number", () => {
+    expect(() => {
+      evaluate("29");
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported Yocto-JavaScript feature: Literal"`
+    );
+  });
+
+  test("Variable declaration", () => {
+    expect(() => {
+      evaluate("const f = x => x");
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported Yocto-JavaScript feature: VariableDeclarator"`
+    );
+  });
+});
+
+test("§ Programs That Do Not Terminate", () => {
+  expect(() => {
+    evaluate("(f => f(f))(f => f(f))");
+  }).toThrowErrorMatchingInlineSnapshot(`"Maximum call stack size exceeded"`);
+  expect(() => {
+    evaluate("(f => (f(f))(f(f)))(f => (f(f))(f(f)))");
+  }).toThrowErrorMatchingInlineSnapshot(`"Maximum call stack size exceeded"`);
+  expect(() => {
+    evaluate("(f => u => f(f)(x => x))(f => u => f(f)(x => x))(x => x)");
+  }).toThrowErrorMatchingInlineSnapshot(`"Maximum call stack size exceeded"`);
+});
+
+test("§ A Function Body Is Evaluated with the Environment in Its Closure", () => {
+  expect(evaluate("(f => (x => f(x))(a => a))((x => z => x)(y => y))"))
+    .toMatchInlineSnapshot(`
+    "{
+      \\"value\\": {
+        \\"function\\": \\"y => y\\",
+        \\"environment\\": []
+      },
+      \\"store\\": [
+        [
+          0,
+          {
+            \\"function\\": \\"y => y\\",
+            \\"environment\\": []
+          }
+        ],
+        [
+          1,
+          {
+            \\"function\\": \\"z => x\\",
+            \\"environment\\": [
+              [
+                \\"x\\",
+                0
+              ]
+            ]
+          }
+        ],
+        [
+          2,
+          {
+            \\"function\\": \\"a => a\\",
+            \\"environment\\": [
+              [
+                \\"f\\",
+                1
+              ]
+            ]
+          }
+        ],
+        [
+          3,
+          {
+            \\"function\\": \\"a => a\\",
+            \\"environment\\": [
+              [
+                \\"f\\",
+                1
+              ]
+            ]
+          }
+        ]
+      ]
+    }"
+  `);
+});
---
+++ code/src/developing-an-analyzer/step-2--store-based-interpreter.ts
@@ -0,0 +1,125 @@
+import * as esprima from "esprima";
+import * as estree from "estree";
+import * as escodegen from "escodegen";
+import * as prettier from "prettier";
+import { MapDeepEqual } from "collections-deep-equal";
+
+export function evaluate(input: string): string {
+  return stringify(run(parse(input)));
+}
+
+type Expression = ArrowFunctionExpression | CallExpression | Identifier;
+
+type ArrowFunctionExpression = {
+  type: "ArrowFunctionExpression";
+  params: [Identifier];
+  body: Expression;
+};
+
+type CallExpression = {
+  type: "CallExpression";
+  callee: Expression;
+  arguments: [Expression];
+};
+
+type Identifier = {
+  type: "Identifier";
+  name: string;
+};
+
+type Value = Closure;
+
+type Closure = {
+  function: ArrowFunctionExpression;
+  environment: Environment;
+};
+
+type Environment = MapDeepEqual<Identifier["name"], Address>;
+
+type Store = MapDeepEqual<Address, Value>;
+
+type Address = number;
+
+function run(expression: Expression): { value: Value; store: Store } {
+  const store: Store = new MapDeepEqual();
+  return { value: step(expression, new MapDeepEqual()), store };
+  function step(expression: Expression, environment: Environment): Value {
+    switch (expression.type) {
+      case "ArrowFunctionExpression": {
+        return { function: expression, environment };
+      }
+      case "CallExpression": {
+        const {
+          function: {
+            params: [parameter],
+            body,
+          },
+          environment: functionEnvironment,
+        } = step(expression.callee, environment);
+        const argument = step(expression.arguments[0], environment);
+        const address = store.size;
+        store.set(address, argument);
+        return step(
+          body,
+          new MapDeepEqual(functionEnvironment).set(parameter.name, address)
+        );
+      }
+      case "Identifier": {
+        const address = environment.get(expression.name);
+        if (address === undefined)
+          throw new Error(
+            `Reference to undefined variable: ${expression.name}`
+          );
+        return store.get(address)!;
+      }
+    }
+  }
+}
+
+function parse(input: string): Expression {
+  const program = esprima.parseScript(input, {}, checkFeatures);
+  const expression = (program as any).body[0].expression as Expression;
+  return expression;
+  function checkFeatures(node: estree.Node): void {
+    switch (node.type) {
+      case "Program":
+        if (node.body.length !== 1)
+          throw new Error(
+            "Unsupported Yocto-JavaScript feature: Program with multiple statements"
+          );
+        break;
+      case "ExpressionStatement":
+        break;
+      case "ArrowFunctionExpression":
+        break;
+      case "CallExpression":
+        if (node.arguments.length !== 1)
+          throw new Error(
+            "Unsupported Yocto-JavaScript feature: CallExpression with multiple arguments"
+          );
+        break;
+      case "Identifier":
+        break;
+      default:
+        throw new Error(`Unsupported Yocto-JavaScript feature: ${node.type}`);
+    }
+  }
+}
+
+function stringify(value: any): string {
+  return JSON.stringify(
+    value,
+    (key, value) => {
+      if (value.type !== undefined)
+        return prettier
+          .format(escodegen.generate(value), {
+            parser: "babel",
+            semi: false,
+            arrowParens: "avoid",
+          })
+          .trim();
+      return value;
+    },
+    2
+  );
+}
