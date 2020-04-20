ad37f12b8e0f77c6f6c51887dc64d1759332a5be
Leandro Facchinetti <git@leafac.com>
2020-04-20 18:40:16 -0400

    .

--- dissertation-new/gatsby-node.js
+++ dissertation-new/gatsby-node.js
@@ -1,13 +1,13 @@
 const Prince = require("prince");
 
 exports.onPostBuild = async () => {
-  try {
-    await new Prince()
-      .option("pdf-profile", "PDF/A-1b")
-      .inputs("public/index.html")
-      .output("yocto-cfa.pdf")
-      .execute();
-  } catch ({ error }) {
-    throw new Error(error);
-  }
+  await new Prince()
+    .option("pdf-profile", "PDF/A-1b")
+    .option("fileroot", `${__dirname}/public`)
+    .inputs("public/index.html")
+    .output("yocto-cfa.pdf")
+    .execute()
+    .catch(({ error }) => {
+      throw new Error(error);
+    });
 };
--- dissertation-new/src/components/layout.js
+++ dissertation-new/src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-// import "typeface-pt-serif";
-// import "typeface-pt-mono";
+import "typeface-pt-serif";
+import "typeface-pt-mono";
 import "./layout.css";
 
 export default ({
