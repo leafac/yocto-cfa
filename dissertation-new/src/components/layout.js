8efa19055c40e42cdaf728faa826e967cbe1a884
Leandro Facchinetti <git@leafac.com>
2020-04-20 19:26:40 -0400

    .

--- dissertation-new/src/components/layout.js
+++ dissertation-new/src/components/layout.js
@@ -7,44 +7,21 @@ import "./layout.css";
 export default ({
   children,
   pageContext: {
-    frontmatter: { title, author, subject, keywords, date },
+    frontmatter: { title, ...meta },
   },
 }) => {
   return (
     <>
       <Helmet>
         <title>{title}</title>
-        <meta name="author" content={author} />
-        <meta name="subject" content={subject} />
-        <meta name="keywords" content={keywords} />
+        {Object.entries(meta).map(([name, content]) => (
+          <meta key={name} name={name} content={content} />
+        ))}
         <meta
           name="date"
           content={new Date().toISOString().slice(0, "yyyy-mm-dd".length)}
         />
-        <meta name="generator" content="Gatsby & Prince" />
       </Helmet>
-      <header>
-        <div class="title-page">
-          <p class="title">{title}</p>
-          <p class="author">
-            by
-            <br />
-            {author}
-          </p>
-          <p class="statement">
-            A dissertation submitted to Johns Hopkins University
-            <br />
-            in conformity with the requirements for the degree of Doctor of
-            Philosophy
-          </p>
-          <p class="publishing-location">
-            Baltimore, Maryland
-            <br />
-            {date}
-          </p>
-          <p>{/* TODO */ new Date().toISOString()}</p>
-        </div>
-      </header>
       {children}
     </>
   );
