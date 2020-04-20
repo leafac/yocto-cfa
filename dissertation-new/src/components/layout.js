b5ba08ad2e1c3b4c994fb3850498391fef5be561
Leandro Facchinetti <git@leafac.com>
2020-04-20 19:50:49 -0400

    .

--- dissertation-new/src/components/layout.js
+++ dissertation-new/src/components/layout.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import "typeface-pt-serif";
+import "typeface-pt-mono";
+import "../components/layout.css";
 
 export default ({
   children,
