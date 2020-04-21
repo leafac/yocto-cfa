41db553997bf6bc0fd8bae6fb14460db4d7db44f
Leandro Facchinetti <git@leafac.com>
2020-04-21 19:52:19 -0400

    .

---
+++ dissertation/gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+          gatsbyRemarkPlugins: [
+            {
+              resolve: `gatsby-remark-vscode`,
+              options: {
+                theme: "Light+ (default light)",
+                injectStyles: false,
+              },
+            },
+          ],
+          remarkPlugins: [require("remark-math")],
+          rehypePlugins: [require("rehype-katex")],
+        },
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
+};
---
+++ dissertation/src/components/layout.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default ({ children }) => <div id="hello">{children}</div>;
