32586c194a5abf09dcae81c98f6f8e59125cf6d9
Leandro Facchinetti <git@leafac.com>
2020-04-23 15:21:47 -0400

    .

--- dissertation/src/scripts.js
+++ dissertation/src/scripts.js
@@ -9,66 +9,67 @@ document
     `<p class="draft">${new Date().toISOString()}</p>`
   );
 
-// Slugify headings & collect table of contents + references
+// Slugify headings
 
-const tableOfContents = [];
-const references = {};
-const referencesCounter = [];
 const slugger = new GithubSlugger();
-for (const element of document.querySelectorAll("h1, h2, h3, h4, h5, h6")) {
+document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach((element) => {
   element.id = slugger.slug(element.textContent);
-  if (element.closest("main") !== null) {
+});
+
+// Number headings
+
+const headingsCounter = [];
+document
+  .querySelectorAll("main h1, main h2, main h3, main h4, main h5, main h6")
+  .forEach((element) => {
     const level = Number(element.tagName[1]);
-    referencesCounter.splice(level);
-    while (referencesCounter.length < level) referencesCounter.push(0);
-    referencesCounter[referencesCounter.length - 1]++;
-    const referencesCounterString = referencesCounter.join(".");
-    element.insertAdjacentHTML(
-      "afterbegin",
-      `<span class="heading-counter">${referencesCounterString}</span> `
-    );
-    element.insertAdjacentHTML(
-      "beforeend",
-      ` <code class="draft">(#${element.id})</code>`
-    );
-    references[element.id] = referencesCounterString;
-  }
-  tableOfContents.push({
-    id: element.id,
-    section: ["header", "main", "footer"].find(
-      (section) => element.closest(section) !== null
-    ),
-    innerHTML: element.innerHTML,
+    headingsCounter.splice(level);
+    while (headingsCounter.length < level) headingsCounter.push(0);
+    headingsCounter[headingsCounter.length - 1]++;
+    element.innerHTML = `<span class="heading-counter">${headingsCounter.join(
+      "."
+    )}</span> <span class="heading-content">${
+      element.innerHTML
+    }</span><code class="draft"> (#${element.id})</code>`;
   });
-}
 
 // Resolve references
 
-for (const element of document.querySelectorAll(`a[href^="#"]`)) {
+document.querySelectorAll(`a[href^="#"]`).forEach((element) => {
   const href = element.getAttribute("href");
-  let reference = references[href.slice(1)];
-  if (reference === undefined) {
+  const target = document.querySelector(href);
+  if (target !== null) {
+    element.innerHTML = `§ ${
+      target.querySelector(".heading-counter").textContent
+    }<span class="draft"> (${
+      target.querySelector(".heading-content").innerHTML
+    })</span>`;
+  } else {
     console.error(`Undefined reference: ${href}`);
-    reference = "??";
+    element.innerHTML = `§ ??`;
   }
-  element.innerHTML = `§ ${reference}`;
-}
+});
 
 // Add table of contents
 
-document
-  .querySelector(".table-of-contents")
-  .insertAdjacentHTML(
-    "afterbegin",
-    tableOfContents
-      .map(
-        ({ id, section, innerHTML }) =>
-          `<li><a href="#${id}" data-section="${section}">${innerHTML}</a></li>`
-      )
-      .join("")
-  );
+document.querySelector(".table-of-contents").innerHTML = [
+  ...document.querySelectorAll("h1, h2, h3, h4, h5, h6"),
+]
+  .map(
+    (element) =>
+      `<li><a href="#${element.id}" data-section="${[
+        "header",
+        "main",
+        "footer",
+      ].find((section) => element.closest(section) !== null)}">${
+        element.innerHTML
+      }</a></li>`
+  )
+  .join("");
 
 // Remove draft
 
 if (document.body.dataset.draft !== "true")
-  for (const element of document.querySelectorAll(".draft")) element.remove();
+  document.querySelectorAll(".draft").forEach((element) => {
+    element.remove();
+  });
