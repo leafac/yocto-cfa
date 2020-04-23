b3ec939c2cdf001aff8cabd2c20811760f6ed448
Leandro Facchinetti <git@leafac.com>
2020-04-23 05:35:24 -0400

    .

--- dissertation/gatsby-node.js
+++ dissertation/gatsby-node.js
@@ -17,36 +17,7 @@ exports.createPages = async ({ graphql }) => {
   `);
   fs.writeFileSync("public/yocto-cfa-raw.html", html);
   const dom = new JSDOM(html);
-  const document = dom.window.document;
-  const headingsCounter = [];
-  for (const element of document.querySelectorAll(
-    "main h1, main h2, main h3, main h4, main h5, main h6"
-  )) {
-    const level = Number(element.tagName[1]);
-    headingsCounter.splice(level);
-    while (headingsCounter.length < level) headingsCounter.push(0);
-    headingsCounter[headingsCounter.length - 1]++;
-    const headingsCounterString = headingsCounter.join(".");
-    const className = "heading-counter";
-    element.insertAdjacentHTML(
-      "afterbegin",
-      `<span class="${className}" id="${element.id}">${headingsCounterString}</span> `
-    );
-    document
-      .querySelector(`.toc [href="#${element.id}"]`)
-      .insertAdjacentHTML(
-        "afterbegin",
-        `<span class="${className}">${headingsCounterString}</span> `
-      );
-    element.removeAttribute("id");
-  }
-  for (const element of document.querySelectorAll(".toc a"))
-    element.dataset.section = ["header", "main", "footer"].find(
-      (section) =>
-        document
-          .querySelector(element.getAttribute("href"))
-          .closest(section) !== null
-    );
+  for (const processor of processors) processor(dom.window.document);
   fs.writeFileSync(
     "public/yocto-cfa.html",
     prettier.format(dom.serialize(), { parser: "html" })
@@ -55,3 +26,40 @@ exports.createPages = async ({ graphql }) => {
     "node_modules/prince/prince/lib/prince/bin/prince --baseurl src/ --pdf-profile PDF/A-1b --no-artificial-fonts --fail-dropped-content --fail-missing-resources --fail-missing-glyphs public/yocto-cfa.html --output yocto-cfa.pdf"
   );
 };
+
+const processors = [
+  function addHeadingsCounter(document) {
+    const headingsCounter = [];
+    for (const element of document.querySelectorAll(
+      "main h1, main h2, main h3, main h4, main h5, main h6"
+    )) {
+      const level = Number(element.tagName[1]);
+      headingsCounter.splice(level);
+      while (headingsCounter.length < level) headingsCounter.push(0);
+      headingsCounter[headingsCounter.length - 1]++;
+      const headingsCounterString = headingsCounter.join(".");
+      const className = "heading-counter";
+      element.insertAdjacentHTML(
+        "afterbegin",
+        `<span class="${className}" id="${element.id}">${headingsCounterString}</span> `
+      );
+      document
+        .querySelector(`.toc [href="#${element.id}"]`)
+        .insertAdjacentHTML(
+          "afterbegin",
+          `<span class="${className}">${headingsCounterString}</span> `
+        );
+      element.removeAttribute("id");
+    }
+  },
+
+  function addSectionInformationOnTableOfContentsEntries(document) {
+    for (const element of document.querySelectorAll(".toc a"))
+      element.dataset.section = ["header", "main", "footer"].find(
+        (section) =>
+          document
+            .querySelector(element.getAttribute("href"))
+            .closest(section) !== null
+      );
+  },
+];
