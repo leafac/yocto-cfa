988c943e5ce88b3c51b868e688eb4534d9ab48c9
Leandro Facchinetti <git@leafac.com>
2020-04-23 05:10:27 -0400

    .

--- dissertation/gatsby-node.js
+++ dissertation/gatsby-node.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const child_process = require("child_process");
+const { JSDOM } = require("jsdom");
+const prettier = require("prettier");
 
 exports.createPages = async ({ graphql }) => {
   const {
@@ -13,7 +15,35 @@ exports.createPages = async ({ graphql }) => {
       }
     }
   `);
-  fs.writeFileSync("public/yocto-cfa.html", html);
+  fs.writeFileSync("public/yocto-cfa-raw.html", html);
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+  const headingsCounter = [];
+  for (const element of document.querySelectorAll(
+    "main h1, main h2, main h3, main h4, main h5, main h6"
+  )) {
+    const level = Number(element.tagName[1]);
+    headingsCounter.splice(level);
+    while (headingsCounter.length < level) headingsCounter.push(0);
+    headingsCounter[headingsCounter.length - 1]++;
+    const headingsCounterString = headingsCounter.join(".");
+    const className = "heading-counter";
+    element.insertAdjacentHTML(
+      "afterbegin",
+      `<span class="${className}" id="${element.id}">${headingsCounterString}</span> `
+    );
+    document
+      .querySelector(`.toc [href="#${element.id}"]`)
+      .insertAdjacentHTML(
+        "afterbegin",
+        `<span class="${className}">${headingsCounterString}</span> `
+      );
+    element.removeAttribute("id");
+  }
+  fs.writeFileSync(
+    "public/yocto-cfa.html",
+    prettier.format(dom.serialize(), { parser: "html" })
+  );
   child_process.execSync(
     "node_modules/prince/prince/lib/prince/bin/prince --baseurl src/ --pdf-profile PDF/A-1b --no-artificial-fonts --fail-dropped-content --fail-missing-resources --fail-missing-glyphs public/yocto-cfa.html --output yocto-cfa.pdf"
   );
