5ac6a01d223960fd550543cfb697f16f9e3a6afa
Leandro Facchinetti <git@leafac.com>
2020-05-03 14:35:13 -0400

    .

--- dissertation/yocto-cfa.js
+++ dissertation/yocto-cfa.js
@@ -50,70 +50,6 @@ async function processHTML(/** @type {Document} */ document) {
       `<div class="timestamp draft">${timestamp}</div>`
     );
 
-  // Add heading counter
-  const counter = [];
-  for (const element of document.querySelectorAll(
-    "main h1, main h2, main h3, main h4, main h5, main h6"
-  )) {
-    const level = element.tagName[1];
-    while (counter.length < level) counter.push(0);
-    counter.splice(level);
-    counter[level - 1]++;
-    const counterString = counter.join(".");
-    element.innerHTML = `<span class="heading-counter">${counterString}</span> ${element.innerHTML}<code class="draft"> (#${element.id})</code>`;
-  }
-
-  // Add Table of Contents
-  document.querySelector("#table-of-contents").insertAdjacentHTML(
-    "afterend",
-    [...document.querySelectorAll("h1, h2, h3, h4, h5, h6")]
-      .filter((header) => header.textContent !== "Table of Contents")
-      .map((header) => {
-        const section = ["header", "main", "footer"].find(
-          (section) => header.closest(section) !== null
-        );
-        return `<div class="table-of-contents-item"><a href="#${header.id}" data-section="${section}">${header.innerHTML}</a></div>`;
-      })
-      .join("")
-  );
-
-  // Resolve cross-references
-  for (const element of document.querySelectorAll(`main a[href^="#"]`)) {
-    const href = element.getAttribute("href");
-    const target = document.querySelector(`${href} .heading-counter`);
-    if (target === null) console.error(`Undefined cross-reference: ${href}`);
-    element.textContent = `§ ${target?.textContent ?? "??"}`;
-  }
-
-  // Resolve citations
-  const unusedCitations = new Set(
-    [...document.querySelectorAll("#bibliography + ol span:first-child")].map(
-      (element) => element.id
-    )
-  );
-  for (const element of document.querySelectorAll(`a[href=""]`)) {
-    const citations = [];
-    for (const segment of element.textContent.split(",")) {
-      const [id, ...description] = segment.trim().split(" ");
-      const target = document.querySelector(`#${id}`);
-      if (target === null) {
-        console.error(`Undefined citation: ${id}`);
-        citations.push(["??", ...description].join(" "));
-        continue;
-      }
-      unusedCitations.delete(id);
-      const listItem = target.parentElement;
-      const index = [...listItem.parentElement.children].indexOf(listItem);
-      const number = index + 1;
-      citations.push(
-        `<a href="#${id}">${[number, ...description].join(" ")}</a>`
-      );
-    }
-    element.outerHTML = `[${citations.join(", ")}]`;
-  }
-  if (unusedCitations.size !== 0)
-    console.error(`Unused citations: ${[...unusedCitations].join(", ")}`);
-
   // Render mathematics
   document.head.insertAdjacentHTML(
     "beforeend",
@@ -238,6 +174,70 @@ async function processHTML(/** @type {Document} */ document) {
   for (const element of document.querySelectorAll("a"))
     if (element.innerHTML === element.getAttribute("href"))
       element.innerHTML = `<code>${element.innerHTML}</code>`;
+
+  // Add heading counter
+  const counter = [];
+  for (const element of document.querySelectorAll(
+    "main h1, main h2, main h3, main h4, main h5, main h6"
+  )) {
+    const level = element.tagName[1];
+    while (counter.length < level) counter.push(0);
+    counter.splice(level);
+    counter[level - 1]++;
+    const counterString = counter.join(".");
+    element.innerHTML = `<span class="heading-counter">${counterString}</span> ${element.innerHTML}<code class="draft"> (#${element.id})</code>`;
+  }
+
+  // Add Table of Contents
+  document.querySelector("#table-of-contents").insertAdjacentHTML(
+    "afterend",
+    [...document.querySelectorAll("h1, h2, h3, h4, h5, h6")]
+      .filter((header) => header.textContent !== "Table of Contents")
+      .map((header) => {
+        const section = ["header", "main", "footer"].find(
+          (section) => header.closest(section) !== null
+        );
+        return `<div class="table-of-contents-item"><a href="#${header.id}" data-section="${section}">${header.innerHTML}</a></div>`;
+      })
+      .join("")
+  );
+
+  // Resolve cross-references
+  for (const element of document.querySelectorAll(`main a[href^="#"]`)) {
+    const href = element.getAttribute("href");
+    const target = document.querySelector(`${href} .heading-counter`);
+    if (target === null) console.error(`Undefined cross-reference: ${href}`);
+    element.textContent = `§ ${target?.textContent ?? "??"}`;
+  }
+
+  // Resolve citations
+  const unusedCitations = new Set(
+    [...document.querySelectorAll("#bibliography + ol span:first-child")].map(
+      (element) => element.id
+    )
+  );
+  for (const element of document.querySelectorAll(`a[href=""]`)) {
+    const citations = [];
+    for (const segment of element.textContent.split(",")) {
+      const [id, ...description] = segment.trim().split(" ");
+      const target = document.querySelector(`#${id}`);
+      if (target === null) {
+        console.error(`Undefined citation: ${id}`);
+        citations.push(["??", ...description].join(" "));
+        continue;
+      }
+      unusedCitations.delete(id);
+      const listItem = target.parentElement;
+      const index = [...listItem.parentElement.children].indexOf(listItem);
+      const number = index + 1;
+      citations.push(
+        `<a href="#${id}">${[number, ...description].join(" ")}</a>`
+      );
+    }
+    element.outerHTML = `[${citations.join(", ")}]`;
+  }
+  if (unusedCitations.size !== 0)
+    console.error(`Unused citations: ${[...unusedCitations].join(", ")}`);
 
   // Remove draft
   if (process.env.NODE_ENV === "production")
